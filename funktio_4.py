006a48a48190e5e8fa49cbae96784d6baee59c3c
Miikka Maki-Uuro <miikka.maki-uuro@metropolia.fi>
2022-09-07 14:12:32 +0300

    eka commit

---
+++ funktio_4.py
@@ -0,0 +1,12 @@
+def laskeKolmionAla(kanta, korkeus):
+    A = (kanta * korkeus) / 2
+    return A
+
+ka = float(input("Anna kolmion kanta   : "))
+ko = float(input("Anna kolmion korkeus : "))
+pintaAla = laskeKolmionAla(ka, ko)
+print(f"Kolmion ala on {pintaAla:.2f}")
+
+
+
+
